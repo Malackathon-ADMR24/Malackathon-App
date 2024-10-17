9371a7068e1786116843d08c8c04f09b267a967f
nightroy99 <arroyotorresdiego@gmail.com>
2024-10-17 18:21:14 +0200

    Añadido una página para las listas de embalses

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,22 @@ import flask
 
 from .usecase import contactoperations
 
+mock_embalse = {
+    "nombre": "Pepito",
+    "ccaa": "Andalucía",
+    "provincia": "Málaga",
+    "coordenadas": {"x": 36.7178, "y": -4.4256},
+    "demarcacion": "Demacración",
+    "agua_total": 100
+}
+mock_embalse2 = {
+    "nombre": "Jorgito",
+    "ccaa": "Andalucía",
+    "provincia": "Málaga",
+    "coordenadas": {"x": 36.7178, "y": -4.4256},
+    "demarcacion": "Demacración",
+    "agua_total": 100
+}
 
 def setup(app):
     @app.get("/")
@@ -11,6 +27,15 @@ def setup(app):
     @app.get("/map")
     def map():
         return flask.render_template("map.html")
+
+    @app.get("/embalse/")
+    def lista_embalse():
+        return flask.render_template("lista_embalse.html", embalses=[mock_embalse, mock_embalse2])
+    
+    @app.get("/embalse/<int:embalse_id>")
+    def detalle_embalse(embalse_id: int):
+        
+        return flask.render_template("detalle_embalse.html", embalse=mock_embalse)
 
     @app.get("/data/contacts")
     def get_contacts():
